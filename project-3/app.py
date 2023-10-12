948828b876cf9cf0390004dc36b577a0afbcb46d
Devin Trapp <devintrapp@Devins-Laptop.local>
2023-10-11 18:34:03 -0700

    recent update

--- project-3/app.py
+++ project-3/app.py
@@ -52,7 +52,7 @@ ranking = Base.classes.ranking
 def static_files(filename):
     return send_from_directory('static', filename)
 
-@app.route("/home")
+@app.route("/")
 def home():
     return render_template("index.html")
 
